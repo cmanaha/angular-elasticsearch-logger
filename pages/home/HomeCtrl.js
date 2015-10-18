c1ca9e2f954da66dd5a6c1dbfa431f904ce76aa5
Carlos Manzanedo Rueda <c.manaha@gmail.com>
2015-10-18 02:28:44 +0100

    Pages deleted, ready to start with the next phase of the project

--- pages/home/HomeCtrl.js
+++
@@ -1,8 +0,0 @@
-/**
-*/
-
-'use strict';
-
-angular.module('myApp').controller('HomeCtrl', ['$scope', 'cmrElasticsearchLogger', function($scope, cmrElasticsearchLogger) {
-	//TODO - put any directive code here
-}]);
