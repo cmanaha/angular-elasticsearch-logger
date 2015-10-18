0e9a99603783db648b51e57c8b662dbf4c1520e5
Carlos Manzanedo Rueda <c.manaha@gmail.com>
2015-10-18 01:13:10 +0100

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'cmrElasticsearchLogger', function($scope, cmrElasticsearchLogger) {
+	//TODO - put any directive code here
+}]);
